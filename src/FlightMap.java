dc2939ec4b6ca0022ba7553107e95b3b0662796a
Raghav Jain <raghavja@usc.edu>
2019-09-22 13:11:17 -0700

    fixed logic for finding path

--- src/FlightMap.java
+++ src/FlightMap.java
@@ -9,64 +9,20 @@ import java.util.Iterator;
 import java.util.Scanner;
 
 public class FlightMap {
-    Pair origin;
-    HashSet<String> destinations;
-    HashMap<String, ArrayList<Pair>> flightInfo;
-    HashMap<String, ArrayList<Pair>> result;
 
-    public FlightMap() {
-        destinations = new HashSet<String>();
-        flightInfo = new HashMap<String, ArrayList<Pair>>();
-        result = new HashMap<String, ArrayList<Pair>>();
-    }
-
-    public void readInput(File input) throws FileNotFoundException {
-        Scanner scanner = new Scanner(input);
-        String originCity = scanner.nextLine();
-        origin = new Pair(originCity, 0);
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            readInputHelper(line);
-        }
-        scanner.close();
-        destinations.remove(origin.city);
-    }
-
-    private void readInputHelper(String line) {
-        String[] lineData = line.split(" ");
-
-        String start = lineData[0];
-        String dest = lineData[1];
-        int cost = Integer.parseInt(lineData[2]);
-
-        ArrayList<Pair> list;
-        if (flightInfo.get(start) == null) {
-            list = new ArrayList<Pair>();
-        } else {
-            list = flightInfo.get(start);
-        }
-
-        Pair pair = new Pair(dest, cost);
-        list.add(pair);
-        flightInfo.put(start, list);
-
-        destinations.add(dest);
-        destinations.add(start);
-    }
-
-    public void parseFlightInfo() {
+    public void parseFlightInfo(HashSet<String> destinations, Pair origin,HashMap<String, ArrayList<Pair>> result, HashMap<String, ArrayList<Pair>> flightInfo) {
         for (String dest : destinations) {
             ArrayList<Pair> solution = new ArrayList<Pair>();
             ArrayList<String> visited = new ArrayList<String>();
 
-            if (parseHelper(origin.city, dest, solution, visited)) {
+            if (parseHelper(origin.city, dest, solution, visited, flightInfo)) {
                 solution.add(solution.size(), origin);
                 result.put(dest, solution);
             }
         }
     }
 
-    private Boolean parseHelper(String start, String dest, ArrayList<Pair> solution, ArrayList<String> visited) {
+    private Boolean parseHelper(String start, String dest, ArrayList<Pair> solution, ArrayList<String> visited, HashMap<String, ArrayList<Pair>> flightInfo) {
         if (start.equals(dest)) {
             return true;
         }
@@ -79,40 +35,11 @@ public class FlightMap {
         }
 
         for (Pair neighbor : neighbors) {
-            if (!visited.contains(neighbor.city) && parseHelper(neighbor.city, dest, solution, visited)) {
+            if (!visited.contains(neighbor.city) && parseHelper(neighbor.city, dest, solution, visited, flightInfo)) {
                 solution.add(neighbor);
                 return true;
             }
         }
-
         return false;
-    }
-
-    public void writeOutput(File output) throws IOException {
-        FileWriter writer = new FileWriter(output);
-        Iterator<String> it = result.keySet().iterator();
-
-        writer.write(String.format("%s %20s %15s\r\n", "Destination", "Flight Route From", "Total Cost"));
-
-        while (it.hasNext()) {
-            String dest = it.next();
-            ArrayList<Pair> list = result.get(dest);
-            int cost = 0;
-            String path = "";
-
-            for (int i = list.size() - 1; i >= 0; i--) {
-                Pair curr = list.get(i);
-                cost += curr.cost;
-                if (i == 0) {
-                    path += curr.city;
-                } else {
-                    path += curr.city + ",";
-                }
-            }
-
-            writer.write(String.format("%s %25s %20d\r\n", dest, path, cost));
-        }
-
-        writer.close();
     }
 }
--- src/SearchMap.java
+++ src/SearchMap.java
@@ -1,16 +1,99 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class SearchMap {
+    static Pair origin;
+    static HashSet<String> destinations;
+    static HashMap<String, ArrayList<Pair>> flightInfo;
+    static HashMap<String, ArrayList<Pair>> result;
+
+    public SearchMap() {
+        destinations = new HashSet<String>();
+        flightInfo = new HashMap<String, ArrayList<Pair>>();
+        result = new HashMap<String, ArrayList<Pair>>();
+    }
+    
+    public static void readInput(File input) throws FileNotFoundException {
+        Scanner scanner = new Scanner(input);
+        String originCity = scanner.nextLine();
+        origin = new Pair(originCity, 0);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            readInputHelper(line);
+        }
+        scanner.close();
+        destinations.remove(origin.city);
+    }
+
+    private static void readInputHelper(String line) {
+        String[] lineData = line.split(" ");
+
+        String start = lineData[0];
+        String dest = lineData[1];
+        int cost = Integer.parseInt(lineData[2]);
+
+        ArrayList<Pair> list;
+
+        if (flightInfo.get(start) == null) {
+            list = new ArrayList<Pair>();
+        } else {
+            list = flightInfo.get(start);
+        }
+
+        Pair pair = new Pair(dest, cost);
+        list.add(pair);
+        flightInfo.put(start, list);
+
+        destinations.add(dest);
+        destinations.add(start);
+    }
+
+    public static void writeOutput(File output) throws IOException {
+        FileWriter writer = new FileWriter(output);
+        Iterator<String> it = result.keySet().iterator();
+
+        writer.write(String.format("%s %20s %15s\r\n", "Destination", "Flight Route From", "Total Cost"));
+
+        while (it.hasNext()) {
+            String dest = it.next();
+            ArrayList<Pair> list = result.get(dest);
+            int cost = 0;
+            String path = "";
+
+            for (int i = list.size() - 1; i >= 0; i--) {
+                Pair curr = list.get(i);
+                cost += curr.cost;
+                if (i == 0) {
+                    path += curr.city;
+                } else {
+                    path += curr.city + ",";
+                }
+            }
+
+            writer.write(String.format("%s %25s %20d\r\n", dest, path, cost));
+        }
+
+        writer.close();
+    }
+
     public static void main(String[] args) {
+        SearchMap searchMap = new SearchMap();
         FlightMap flightMap = new FlightMap();
 
         File input = new File(args[0]);
         File output = new File(args[1]);
 
         try {
-            flightMap.readInput(input);
-            flightMap.parseFlightInfo();
-            flightMap.writeOutput(output);
+            searchMap.readInput(input);
+            flightMap.parseFlightInfo(searchMap.destinations, searchMap.origin, searchMap.result, searchMap.flightInfo);
+            searchMap.writeOutput(output);
         } catch (Exception e) {
             e.printStackTrace();
         }
